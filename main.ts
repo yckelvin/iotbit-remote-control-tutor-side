a533ece30e0de44463f3c9e3be3def9a5b78f297
Kelvin Lee <yckelvin@users.noreply.github.com>
2025-02-11 10:26:36 +0800

    update for Caritas Chai Wan Marden Foundation Secondary School

--- main.ts
+++ main.ts
@@ -17,9 +17,19 @@ ESP8266_IoT.MqttEvent("myhome/tutor/remote-control", ESP8266_IoT.QosList.Qos2, f
         pins.digitalWritePin(DigitalPin.P4, 0)
     }
 })
+input.onButtonPressed(Button.A, function () {
+    ESP8266_IoT.publishMqttMessage("1-ON", "myhome/tutor/remote-control", ESP8266_IoT.QosList.Qos2)
+})
+input.onButtonPressed(Button.B, function () {
+    ESP8266_IoT.publishMqttMessage("1-OFF", "myhome/tutor/remote-control", ESP8266_IoT.QosList.Qos2)
+})
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P3, 0)
+pins.digitalWritePin(DigitalPin.P4, 0)
 basic.showNumber(0)
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
-ESP8266_IoT.connectWifi("ssid", "password")
+ESP8266_IoT.connectWifi("RM309", "RM309-y5")
 let clientID = randint(0, 99999999)
 basic.showNumber(1)
 ESP8266_IoT.setMQTT(
@@ -29,7 +39,7 @@ convertToText(clientID),
 "test",
 ""
 )
-ESP8266_IoT.connectMQTT("192.168.0.32", 1884, false)
+ESP8266_IoT.connectMQTT("172.16.82.251", 1884, false)
 basic.showNumber(2)
 OLED.init(128, 64)
 basic.showNumber(3)
@@ -37,3 +47,5 @@ basic.pause(2000)
 if (ESP8266_IoT.isMqttBrokerConnected()) {
     basic.showIcon(IconNames.Yes)
 }
+basic.pause(5000)
+led.enable(false)
